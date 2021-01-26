aed603883b1d74797e65a923470edda206097eae
andijayawizard <andijayawizard@gmail.com>
2021-01-27 00:47:53 +0700

    perubahan folder

---
+++ web/kategori/crud.php
@@ -0,0 +1,52 @@
+<?php
+require '../api/panggil.php';
+
+// proses tambah
+if (!empty($_GET['aksi'] == 'tambah')) {
+	$nama = strip_tags($_POST['nama']);
+	$ket = strip_tags($_POST['ket']);
+	$gambar = strip_tags($_POST['gambar']);
+
+	$tabel = 'kategori';
+	# proses insert
+	$data[] = array(
+		'nama'    => $nama,
+		'ket'    => $ket,
+		'gambar'  => $gambar,
+	);
+	$proses->tambah_data($tabel, $data);
+	echo '<script>alert("Tambah Data Berhasil");window.location="' . $abs . '/kategori"</script>';
+}
+
+// proses edit
+if (!empty($_GET['aksi'] == 'edit')) {
+	$nama = strip_tags($_POST['nama']);
+	$ket = strip_tags($_POST['ket']);
+	$gambar = strip_tags($_POST['gambar']);
+
+	// jika password tidak diisi
+	if (!$pass == '') {
+		$data = array();
+	} else {
+
+		$data = array(
+			'nama'    => $nama,
+			'ket'  => $ket,
+			'gambar'    => $gambar,
+		);
+	}
+	$tabel = 'kategori';
+	$where = 'idkat';
+	$id = strip_tags($_POST['idkat']);
+	$proses->edit_data($tabel, $data, $where, $id);
+	echo '<script>alert("Edit Data Berhasil");window.location="' . $abs . '/kategori"</script>';
+}
+
+// hapus data
+if (!empty($_GET['aksi'] == 'hapus')) {
+	$tabel = 'kategori';
+	$where = 'idkat';
+	$id = strip_tags($_GET['idkat']);
+	$proses->hapus_data($tabel, $where, $id);
+	echo '<script>alert("Hapus Data Berhasil");window.location="' . $abs . '/kategori"</script>';
+}
---
+++ web/proses/crud.php
@@ -0,0 +1,23 @@
+<?php
+    require '../../api/panggil.php';
+
+    // login
+    if(!empty($_GET['aksi'] == 'login'))
+    {   
+        // validasi text untuk filter karakter khusus dengan fungsi strip_tags()
+        $user = strip_tags($_POST['user']);
+        $pass = strip_tags($_POST['pass']);
+        // panggil fungsi proses_login() yang ada di class prosesCrud()
+        $result = $proses->proses_login($user,$pass);
+        if($result == 'gagal')
+        {
+            echo "<script>window.location='../login.php?get=gagal';</script>";
+        }else{
+            // status yang diberikan 
+            session_start();
+            $_SESSION['ADMIN'] = $result;
+            // status yang diberikan 
+            echo "<script>window.location='".$abs."/web/user';</script>";
+        }
+    }
+?>
