0773f8a8009334552498e22e586ff9adf40b8e53
andijaya <andijayawizard@gmail.com>
2021-01-26 17:11:28 +0700

    upload pertama

---
+++ layouts/menu.php
@@ -0,0 +1,3 @@
+<a href="<?= $abs; ?>/user">user</a>
+<a href="<?= $abs; ?>/kategori">kategori</a>
+<a href="<?= $abs; ?>/produk">produk</a>
---
+++ user/panggil.php
@@ -0,0 +1,18 @@
+<?php
+    // panggil file
+    include '../api/koneksi.php';
+    include '../api/prosescrud.php';
+    // cara panggil class di koneksi php
+    $db = new Koneksi();
+    // cara panggil koneksi di fungsi DBConnect()
+    $koneksi =  $db->DBConnect();
+    // panggil class prosesCrud di file prosescrud.php
+    $proses = new prosesCrud($koneksi);
+    // menghilangkan pesan error
+    error_reporting(0);
+    // panggil session ID
+    $id = $_SESSION['ADMIN']['id_login'];
+    $sesi = $proses->tampil_data_id('tbl_user','id_login',$id);
+
+    $abs = 'http://localhost/login-crud';
+?>
